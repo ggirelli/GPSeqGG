0956b92aa9884fee20f0b5abb59ea13a29c627a3
Gabriele Girelli <gigi.ga90@gmail.com>
2017-03-15 10:46:06 +0100

    Log the SAM filter step.

--- modules/main.sam_filter.mod.sh
+++ modules/main.sam_filter.mod.sh
@@ -20,6 +20,13 @@ function filter_sam() {
 	input_int 30 'MAPQ threshold' $mapqThr
 	mapqthr=$v
 
+	# Update summary header
+	new_fields="\tsecondary_aln\tchimeras\tunmapped\tr2\tmapq < $mapqthr\trmChr"
+	new_fields="$new_fields\tafterSAMfilter"
+	head -n 1 $outcontrol/summary_pattern | \
+		awk -v nf="$new_fields" "{ print \$0 nf }" - \
+		> $outcontrol/summary_sam_filter
+
 	for condition in "${condv[@]}"; do
 		echo -e "\nAnalyzing UMIs from condition '$condition'..."
 
@@ -44,7 +51,43 @@ function filter_sam() {
 			-mt $mapqThr -cs $cutsite -c $threads $flags & pid0=$!
 		wait $pid0
 
+		# Update summary -------------------------------------------------------
+
+		# Secondary alignments
+		c=`cat $cout/"$condition"/"$condition".sam_filter_notes.txt | \
+			grep 'secondary alignments' | head -n 1 | cut -d ' ' -f 1`
+
+		# Chimeric reads
+		c2=`cat $cout/"$condition"/"$condition".sam_filter_notes.txt | \
+			grep 'chimeric reads' | head -n 1 | cut -d ' ' -f 1`
+		
+		# Unmapped reads	
+		c3=`cat $cout/"$condition"/"$condition".sam_filter_notes.txt | \
+			grep 'unmapped reads' | head -n 1 | cut -d ' ' -f 1`
+
+		# R2 reads	
+		c4=`cat $cout/"$condition"/"$condition".sam_filter_notes.txt | \
+			grep 'R2 reads' | head -n 1 | cut -d ' ' -f 1`
+
+		# MAPQ < thr reads	
+		c5=`cat $cout/"$condition"/"$condition".sam_filter_notes.txt | \
+			grep 'reads with MAPQ <' | head -n 1 | cut -d ' ' -f 1`
+
+		# Chr removed reads	
+		c6=`cat $cout/"$condition"/"$condition".sam_filter_notes.txt | \
+			grep 'reads from removed chromosomes' | head -n 1 | cut -d ' ' -f 1`
+
+		# Surviving reads	
+		c7=`cat $cout/"$condition"/"$condition".sam_filter_notes.txt | \
+			grep 'reads left after filtering' | head -n 1 | cut -d ' ' -f 1`
+
+		# Add to summary
+		new_fields="\t$c\t$c2\t$c3\t$c4\t$c5\t$c6\t$c7"
+		grep "$condition" "$outcontrol/summary_pattern" | \
+			awk -v nf="$new_fields" "{ print \$0 nf }" - \
+			>> $outcontrol/summary_sam_filter
 	done
+
 }
 
 ################################################################################
