325bafb6b44f059a56b850535540712e58bb591d
Gabriele Girelli <gigi.ga90@gmail.com>
2017-08-30 09:06:14 +0200

    Removed empty bins. Fixed first empty line.

--- tools/centrality_estimate/estimate_centrality.sh
+++ tools/centrality_estimate/estimate_centrality.sh
@@ -437,11 +437,14 @@ done
 # 1   2     3   4        5       6      7         8   9
 # chr|start|end|condRead|readSum|readMu|readSigma|nCS|condID
 comb=$(echo -e "$comb" | gawk -f "$awkdir/add_chr_id.awk" | \
-    sort -k1,1n -k3,3n -k10,10n  | cut -f2- | sed 1d )
+    sort -k1,1n -k3,3n -k10,10n  | cut -f2- )
+
+# Remove first line if empty
+tmp=$(echo -e "$comb" | head -n1)
+if [ -z "$tmp" ]; then comb=$(echo -e "$comb" | sed 1d); fi
 
 # Discard bins with no cutsites/reads from the analysis
-comb=$(echo -e "$comb" | gawk '0 != $8')
-comb=$(echo -e "$comb" | gawk '0 != $5')
+comb=$(echo -e "$comb" | gawk '0 != $8 && 0 != $5')
 
 # 7) Estimate centrality -------------------------------------------------------
 echo -e " Estimating centrality ..."
